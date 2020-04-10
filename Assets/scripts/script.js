e67e98e2f8e35fb17c91e8b8f7608b1dd5827d2d
Andrew Hansbrough <hansbroughku@gmail.com>
2020-04-10 08:52:20 -0500

    Attempted to get question div to replace main div

---
+++ Assets/scripts/script.js
@@ -0,0 +1,7 @@
+//moment().calendar();
+//console.log(moment().calendar());
+//var saveBtn = moment().format("MMMM Do YYYY, h:mm:ss a");
+//console.log(saveBtn);
+//$.each()
+
+//$(moment().format("MMMM Do YYYY, dddd, h:mm:ss a"));
