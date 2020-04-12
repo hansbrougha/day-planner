d593b0ed30b77f031fe360f1f48751ad4ac09ca7
Andrew Hansbrough <hansbroughku@gmail.com>
2020-04-11 22:42:59 -0500

    Added Time Clock using Moment.js

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,7 +1,11 @@
-//moment().calendar();
-//console.log(moment().calendar());
-//var saveBtn = moment().format("MMMM Do YYYY, h:mm:ss a");
-//console.log(saveBtn);
-//$.each()
-
-//$(moment().format("MMMM Do YYYY, dddd, h:mm:ss a"));
+//Clock Element
+var clock = document.getElementById("clock");
+//Time Function
+function updateTime() {
+  var currentTime = moment();
+  var displayFormat = currentTime.format("MMMM Do YYYY, h:mm:ss a");
+  clock.textContent = displayFormat;
+}
+//Time Interval, updates every second.
+setInterval(updateTime, 1000);
+updateTime();
