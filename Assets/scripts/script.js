30b8d2c73d2f1f42b63f2de57eee711898502c37
Andrew Hansbrough <hansbroughku@gmail.com>
2020-04-14 11:29:37 -0500

    Appended New Row to container.

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,79 +1,76 @@
+//Clock Element
+var clock = document.getElementById("clock");
+//Time Function
+function updateTime() {
+  var currentTime = moment();
+  var displayFormat = currentTime.format("MMMM Do YYYY, h:mm:ss a");
+  $("#clock").text(displayFormat);
+}
+//Time Interval, updates every second.
+setInterval(updateTime, 1000);
+
 var currentDay = [
   {
-    order: "0",
-    hour: "9",
-    time: "9",
+    hour: "9:00",
+    time: "09",
     meridiem: "AM",
     task: "",
   },
   {
-    order: "1",
-    hour: "10",
+    hour: "10:00",
     time: "10",
     meridiem: "AM",
     task: "",
   },
   {
-    order: "2",
-    hour: "11",
+    hour: "11:00",
     time: "11",
     meridiem: "AM",
     task: "",
   },
   {
-    order: "3",
-    hour: "12",
+    hour: "12:00",
     time: "12",
     meridiem: "PM",
     task: "",
   },
   {
-    order: "4",
-    hour: "1",
-    time: "1",
+    hour: "1:00",
+    time: "13",
     meridiem: "PM",
     task: "",
   },
   {
-    order: "5",
-    hour: "2",
-    time: "2",
+    hour: "2:00",
+    time: "14",
     meridiem: "PM",
     task: "",
   },
   {
-    order: "6",
-    hour: "3",
-    time: "3",
+    hour: "3:00",
+    time: "15",
     meridiem: "PM",
     task: "",
   },
   {
-    order: "7",
-    hour: "4",
-    time: "4",
+    hour: "4:00",
+    time: "16",
     meridiem: "PM",
     task: "",
   },
   {
-    order: "8",
-    hour: "5",
-    time: "5",
+    hour: "5:00",
+    time: "",
     meridiem: "PM",
     task: "",
   },
 ];
 console.log(currentDay);
 
-//Clock Element
-var clock = document.getElementById("clock");
-console.log(clock);
-//Time Function
-function updateTime() {
-  var currentTime = moment();
-  var displayFormat = currentTime.format("MMMM Do YYYY, h:mm:ss a");
-  clock.textContent = displayFormat;
-}
-//Time Interval, updates every second.
-setInterval(updateTime, 1000);
-updateTime();
+currentDay.forEach(function (currentHour) {
+  //Creates New Row, Appends to container
+  var newRow = $("<form>").attr({
+    class: "row time-block",
+  });
+  $(".container").append(newRow);
+});
