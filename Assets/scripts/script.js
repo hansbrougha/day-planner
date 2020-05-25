9948adbd447a621bc2e0b07522a23f00b6a7d553
Andrew Hansbrough <hansbroughku@gmail.com>
2020-05-25 02:16:21 -0500

    Reworked jQuery in script file from previous state, added bootstrap columns

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -6,126 +6,48 @@ function updateTime() {
   var displayFormat = currentTime.format("MMMM Do YYYY, h:mm:ss a");
   $("#clock").text(displayFormat);
 }
+
 //Time Interval, updates every second.
 setInterval(updateTime, 1000);
 
-var currentDay = [
-  {
-    hour: "9:00",
-    time: "09",
-    meridiem: "AM",
-    task: "",
-  },
-  {
-    hour: "10:00",
-    time: "10",
-    meridiem: "AM",
-    task: "",
-  },
-  {
-    hour: "11:00",
-    time: "11",
-    meridiem: "AM",
-    task: "",
-  },
-  {
-    hour: "12:00",
-    time: "12",
-    meridiem: "PM",
-    task: "",
-  },
-  {
-    hour: "1:00",
-    time: "13",
-    meridiem: "PM",
-    task: "",
-  },
-  {
-    hour: "2:00",
-    time: "14",
-    meridiem: "PM",
-    task: "2 pm task info",
-  },
-  {
-    hour: "3:00",
-    time: "15",
-    meridiem: "PM",
-    task: "",
-  },
-  {
-    hour: "4:00",
-    time: "16",
-    meridiem: "PM",
-    task: "",
-  },
-  {
-    hour: "5:00",
-    time: "17",
-    meridiem: "PM",
-    task: "",
-  },
-];
-console.log(currentDay);
-
-//Saves User Data in Local Storage
-
-//Shows User Data for each hour
-
-currentDay.forEach(function (currentHour) {
-  //console.log(currentHour);
-  //Creates New Row, Appends to container
-  var newRow = $("<form>").attr({
-    class: "row time-block",
+$(document).ready((clock) => {
+  $(".saveBtn").on("click", () => {
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, value);
   });
-  $(".container").append(newRow);
-  //Creates New Div for Time Slot
-  var timeSlot = $("<div>")
-    .text(`${currentHour.hour}${currentHour.meridiem}`)
-    .attr({
-      class: "col-1 hour",
-    });
 
-  //Creates New Text Area for User Data. Doesn't Seem to Work.
-  //Need specific identifier
-  var taskInfo = $("<textarea>");
-  taskInfo.attr({ class: "col-6 description" });
-  taskInfo.attr("task", currentHour.task);
-  taskInfo.attr("id", "input" + currentHour.time);
-  console.log(currentHour.task);
-
-  //Adds Class Past to Text Area when time slot has passed
-  if (currentHour.time < moment().format("HH")) {
-    taskInfo.attr({
-      class: "past",
-    });
-    //If current hour, use class present
-  } else if (currentHour.time === moment().format("HH")) {
-    taskInfo.attr({
-      class: "present",
-    });
-    //Class of future for upcoming tasks
-  } else if (currentHour.time > moment().format("HH")) {
-    taskInfo.attr({
-      class: "future",
+  //Time Function
+  function updateTime() {
+    var currentHour = moment().hours("MMMM Do YYYY, h:mm:ss a");
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
     });
   }
-  // Save Button
-  var saveButton = $("<span class='ui-icon-disk'></span>");
-  var saveTask = $("<button>").attr({
-    class: "col-1 saveBtn",
-  });
-  //Appends saveButton to newRow
-  saveTask.append(saveButton);
-  newRow.append(newRow, timeSlot, taskInfo, saveTask);
+  updateTime();
+  // set up interval to check if current time needs to be updated
+  var interval = setInterval(updateTime, 15000);
+
+  //load data from local storage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  $("#clock").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 });
-//====================================
-//Click Event needs to save input from text area (task)
-//When we click the button, we need a unique identifier on each textarea
-$(".saveBtn").click(function (event) {
-  event.preventDefault();
-  console.log(event);
-});
-//Check all inputs based off their id and see if the values appear in html elements using inspector
-//event listener is going to be applied to every single button
-//try and figure out a way to capture your values.
-//you will need to reference .val somewhere in your code.
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
-I created the clock element in the header with assistance from the Dcode Youtube page.
+I created the clock element in the header with assistance from the Dcode Youtube page. I will spend the week trying the rest of this again.
